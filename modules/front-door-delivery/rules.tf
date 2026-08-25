db0870ab399654016411be66733e28e36a1cf89f
sambez-work <sam.berryman@ukho.gov.uk>
2026-08-25 10:28:42 +0100

    testing actions

--- modules/front-door-delivery/rules.tf
+++ modules/front-door-delivery/rules.tf
@@ -7,61 +7,58 @@ resource "azurerm_cdn_frontdoor_rule_set" "this" {
 
 resource "azurerm_cdn_frontdoor_rule" "this" {
   for_each = var.rules
-
   name                      = each.value.name
   cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.this[each.value.rule_set_key].id
   order                     = each.value.order
   behaviour_on_match        = each.value.behavior_on_match
 
-  actions {
-    dynamic "url_redirect_action" {
-      for_each = each.value.actions.url_redirect_action != null ? [each.value.actions.url_redirect_action] : []
-      content {
-        redirect_type        = url_redirect_action.value.redirect_type
-        redirect_protocol    = url_redirect_action.value.redirect_protocol
-        destination_hostname = url_redirect_action.value.destination_hostname
-        destination_path     = url_redirect_action.value.destination_path
-        query_string         = url_redirect_action.value.query_string
-        destination_fragment = url_redirect_action.value.destination_fragment
-      }
+  dynamic "url_redirect_action" {
+    for_each = each.value.actions != null && each.value.actions.url_redirect_action != null ? [each.value.actions.url_redirect_action] : []
+    content {
+      redirect_type        = url_redirect_action.value.redirect_type
+      redirect_protocol    = url_redirect_action.value.redirect_protocol
+      destination_hostname = url_redirect_action.value.destination_hostname
+      destination_path     = url_redirect_action.value.destination_path
+      query_string         = url_redirect_action.value.query_string
+      destination_fragment = url_redirect_action.value.destination_fragment
     }
+  }
 
-    dynamic "url_rewrite_action" {
-      for_each = each.value.actions.url_rewrite_action != null ? [each.value.actions.url_rewrite_action] : []
-      content {
-        source_pattern          = url_rewrite_action.value.source_pattern
-        destination             = url_rewrite_action.value.destination
-        preserve_unmatched_path = url_rewrite_action.value.preserve_unmatched_path
-      }
+  dynamic "url_rewrite_action" {
+    for_each = each.value.actions != null && each.value.actions.url_rewrite_action != null ? [each.value.actions.url_rewrite_action] : []
+    content {
+      source_pattern          = url_rewrite_action.value.source_pattern
+      destination             = url_rewrite_action.value.destination
+      preserve_unmatched_path = url_rewrite_action.value.preserve_unmatched_path
     }
+  }
 
-    dynamic "route_configuration_override_action" {
-      for_each = each.value.actions.route_configuration_override_action != null ? [each.value.actions.route_configuration_override_action] : []
-      content {
-        cdn_frontdoor_origin_group_id = route_configuration_override_action.value.origin_group_key != null ? azurerm_cdn_frontdoor_origin_group.this[route_configuration_override_action.value.origin_group_key].id : null
-        forwarding_protocol           = route_configuration_override_action.value.forwarding_protocol
-        cache_behavior                = route_configuration_override_action.value.cache_behavior
-        cache_duration                = route_configuration_override_action.value.cache_duration
-        compression_enabled           = route_configuration_override_action.value.compression_enabled
-      }
+  dynamic "route_configuration_override_action" {
+    for_each = each.value.actions != null && each.value.actions.route_configuration_override_action != null ? [each.value.actions.route_configuration_override_action] : []
+    content {
+      cdn_frontdoor_origin_group_id = route_configuration_override_action.value.origin_group_key != null ? azurerm_cdn_frontdoor_origin_group.this[route_configuration_override_action.value.origin_group_key].id : null
+      forwarding_protocol           = route_configuration_override_action.value.forwarding_protocol
+      cache_behavior                = route_configuration_override_action.value.cache_behavior
+      cache_duration                = route_configuration_override_action.value.cache_duration
+      compression_enabled           = route_configuration_override_action.value.compression_enabled
     }
+  }
 
-    dynamic "request_header_action" {
-      for_each = each.value.actions.request_header_actions != null ? each.value.actions.request_header_actions : []
-      content {
-        header_action = request_header_action.value.header_action
-        header_name   = request_header_action.value.header_name
-        value         = request_header_action.value.value
-      }
+  dynamic "request_header_action" {
+    for_each = each.value.actions != null && each.value.actions.request_header_actions != null ? each.value.actions.request_header_actions : []
+    content {
+      header_action = request_header_action.value.header_action
+      header_name   = request_header_action.value.header_name
+      value         = request_header_action.value.value
     }
+  }
 
-    dynamic "response_header_action" {
-      for_each = each.value.actions.response_header_actions != null ? each.value.actions.response_header_actions : []
-      content {
-        header_action = response_header_action.value.header_action
-        header_name   = response_header_action.value.header_name
-        value         = response_header_action.value.value
-      }
+  dynamic "response_header_action" {
+    for_each = each.value.actions != null && each.value.actions.response_header_actions != null ? each.value.actions.response_header_actions : []
+    content {
+      header_action = response_header_action.value.header_action
+      header_name   = response_header_action.value.header_name
+      value         = response_header_action.value.value
     }
   }
 
